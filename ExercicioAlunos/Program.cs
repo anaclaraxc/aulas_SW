292ba18d21df45b957850c9f1cb9aeedd0b06b05
Ana Clara Cordeiro <100931939+anaclaraxc@users.noreply.github.com>
2023-03-14 10:40:32 -0300

    Atividade dia 14-03

---
+++ ExercicioAlunos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = "Catatau Vieira";
+        aluno1.nota1 = 7.4;
+        aluno1.nota2 = 4.6;
+
+        aluno1.mensagem();
+    }
+}
